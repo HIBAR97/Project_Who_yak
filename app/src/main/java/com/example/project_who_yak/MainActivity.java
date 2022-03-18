3a1a612be7de0ce1cb30e901d848a9caf019e0a4
HIBER97 <hibar9710@gmail.com>
2022-03-18 09:12:26 +0900

    2022. 3. 18. AM 9:10 --------------------------------------- ADD - Sync SignUp button - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/MainActivity.java
+++ app/src/main/java/com/example/project_who_yak/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.project_who_yak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
+        Button btnSignUp;
+
+        btnSignUp = (Button)findViewById(R.id.Sigin_Button_ID);
+
+        //Sigin Button Listener
+        btnSignUp.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp_activity.class);
+                startActivity(intent);
+            }
+        });
+
+        
     }
 }
