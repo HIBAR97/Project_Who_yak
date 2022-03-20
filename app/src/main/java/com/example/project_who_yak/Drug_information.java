80b55eadc90486008ba30275dbc5740977bf8062
HIBER97 <hibar9710@gmail.com>
2022-03-20 23:36:42 +0900

    2022. 3. 20. PM 11:34 --------------------------------------- ADD - Drug_information xml, java - --------------------------------------- Delete - - --------------------------------------- Error - Layout complet 20% - fragment requirement prediction --------------------------------------- Fix - - ---------------------------------------

---
+++ app/src/main/java/com/example/project_who_yak/Drug_information.java
@@ -0,0 +1,17 @@
+package com.example.project_who_yak;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Drug_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //화면 이동
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_drug_information);
+
+    }
+}
