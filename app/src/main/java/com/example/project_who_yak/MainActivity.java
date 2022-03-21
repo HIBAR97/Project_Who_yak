18301b5bbf80b8e1c4121b0465e13052b036958b
HIBER97 <hibar9710@gmail.com>
2022-03-21 16:59:00 +0900

    2022. 3. 21. PM 4:58 --------------------------------------- ADD - - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - Mainpage signup button sync restore - ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/MainActivity.java
+++ app/src/main/java/com/example/project_who_yak/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Drug_information.class);
+                Intent intent = new Intent(getApplicationContext(), SignUp_activity.class);
                 startActivity(intent);
             }
         });
