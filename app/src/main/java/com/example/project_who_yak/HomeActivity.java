639904cfa0aad2b74a6e876477c9922e0625100f
joy961027 <joy961027@naver.com>
2022-03-22 13:39:01 +0900

    2022. 03. 22. PM 12":38 --------------------------------------- ADD -MypageActivity - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix -HomeActivity - ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/HomeActivity.java
+++ app/src/main/java/com/example/project_who_yak/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.project_who_yak;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //userpage
+        Button btnUserpage;
+
+        btnUserpage = (Button)findViewById(R.id.btnUserpage);
+
+        //userpage Button Listener
+        btnUserpage.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), UserpageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/project_who_yak/UserpageActivity.java
@@ -0,0 +1,33 @@
+package com.example.project_who_yak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class UserpageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage_profile);
+
+
+    //home
+    Button btnHome;
+
+    btnHome = (Button)findViewById(R.id.btnhome);
+
+    //home Button Listener
+        btnHome.setOnClickListener(new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+        }
+    });
+}
+}
