1067e35f6729583e7df4e5566d963e45df5d19e3
jyun23 <vjaos0619@naver.com>
2022-03-22 13:59:01 +0900

    2022. 3. 22. PM 1:58 --------------------------------------- ADD -버튼 화면이동(홈) - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - --------------------------------------- ```
    -----------------

--- app/src/main/java/com/example/project_who_yak/HomeActivity.java
+++ app/src/main/java/com/example/project_who_yak/HomeActivity.java
@@ -1,16 +1,55 @@
 package com.example.project_who_yak;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button btnSearch;
+    Button btnScan;
+    Button btnSchedule;
+    Button btnBoard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnSearch = findViewById(R.id.btnSearch);
+        btnScan = findViewById(R.id.btnScan);
+        btnSchedule = findViewById(R.id.btnSchedule);
+        btnBoard = findViewById(R.id.btnBoard);
+
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.layout_drug_information);
+            }
+        });
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_camara_scan);
+            }
+        });
+
+        btnSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_schedule);
+            }
+        });
+
+        btnBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_bltboard);
+            }
+        });
     }
 }
