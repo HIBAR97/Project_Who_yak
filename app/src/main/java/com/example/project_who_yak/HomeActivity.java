669b130e3461434993cd4e0fc3c26c1b9cefe976
jyun23 <vjaos0619@naver.com>
2022-03-22 13:48:44 +0900

    2022. 3. 22. PM 1:48 --------------------------------------- ADD -버튼 화면이동(스케줄) - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - --------------------------------------- ```
    -----------------

--- app/src/main/java/com/example/project_who_yak/HomeActivity.java
+++ app/src/main/java/com/example/project_who_yak/HomeActivity.java
@@ -1,34 +1,16 @@
 package com.example.project_who_yak;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        //userpage
-        Button btnUserpage;
-
-        btnUserpage = (Button)findViewById(R.id.btnUserpage);
-
-        //userpage Button Listener
-        btnUserpage.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), UserpageActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
     }
 }
--- app/src/main/java/com/example/project_who_yak/ScheduleActivity.java
+++ app/src/main/java/com/example/project_who_yak/ScheduleActivity.java
@@ -1,14 +1,36 @@
 package com.example.project_who_yak;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class ScheduleActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    Button btnvoice;
+    Button btnhome;
+    ListView lvCal;
+    ListView lvCal2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        btnvoice = findViewById(R.id.btnvoice);
+        btnhome = findViewById(R.id.btnhome);
+
+
+        btnhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_home);
+            }
+        });
     }
 }
