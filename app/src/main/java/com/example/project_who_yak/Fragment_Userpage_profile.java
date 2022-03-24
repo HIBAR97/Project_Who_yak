c0848716a88151d14d5aaef559abe0262a8ae417
joy961027 <joy961027@naver.com>
2022-03-24 12:30:47 +0900

    2022. 03. 24. PM 12:28 --------------------------------------- ADD -(xml) fragment userpage_profile,manual,setting -(java) fragment userpage_profile,manual,setting --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix -(java)UserpageActivity -(xml)activity_userpage ---------------------------------------

---
+++ app/src/main/java/com/example/project_who_yak/Fragment_Userpage_profile.java
@@ -0,0 +1,19 @@
+package com.example.project_who_yak;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment_Userpage_profile extends Fragment {
+    @Nullable
+    //Connect xml file using container
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_userpage_profile, container, false);
+    }
+}
--- app/src/main/java/com/example/project_who_yak/UserpageActivity.java
+++ app/src/main/java/com/example/project_who_yak/UserpageActivity.java
@@ -1,34 +1,93 @@
 package com.example.project_who_yak;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class UserpageActivity extends AppCompatActivity {
+
+
+    //Variable declaration FragmentManager, Fragment page
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
+    private Fragment_Userpage_profile Frag_Userpage_profile;
+    private Fragment_Userpage_manual Frag_Userpage_manual;
+    private Fragment_Userpage_setting Frag_Userpage_setting;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mypage_profile);
+        setContentView(R.layout.activity_userpage);
 
 
-    //home
-    Button btnHome;
+
+        //varivable btn
+        Button btnHome;
+        Button btnProfile;
+        Button btnManual;
+        Button btnSetting;
+
+        //connect btn on layout
+        btnHome = (Button)findViewById(R.id.btnhome);
+        btnProfile = (Button)findViewById(R.id.btnprofile);
+        btnManual = (Button)findViewById(R.id.btnmanual);
+        btnSetting = (Button)findViewById(R.id.btnsetting);
+
+        fragmentManager = getSupportFragmentManager();
 
 
-    btnHome = (Button)findViewById(R.id.btnhome);
+        //Fragment page variable
+        Frag_Userpage_profile = new Fragment_Userpage_profile();
+        Frag_Userpage_manual= new Fragment_Userpage_manual();
+        Frag_Userpage_setting= new Fragment_Userpage_setting();
 
-    //home Button Listener
+        //Default fragment
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.FarmeLayout_Userpage, Frag_Userpage_profile).commitAllowingStateLoss();
+
+
+        //home Button Listener
         btnHome.setOnClickListener(new View.OnClickListener(){
-
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
             startActivity(intent);
         }
     });
+
+        //profile Button Listener
+        btnProfile.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.FarmeLayout_Userpage, Frag_Userpage_profile).commitAllowingStateLoss();
+            }
+        });
+
+        //manual Button Listener
+        btnManual.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.FarmeLayout_Userpage, Frag_Userpage_manual).commitAllowingStateLoss();
+            }
+        });
+
+        //setting Button Listener
+        btnSetting.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.FarmeLayout_Userpage, Frag_Userpage_setting).commitAllowingStateLoss();
+            }
+        });
+
 }
 }
