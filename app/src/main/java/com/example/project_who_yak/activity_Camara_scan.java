c229766944fea4bf9448c0e1d8a07040e0eeabe1
HIBER97 <hibar9710@gmail.com>
2022-03-30 12:22:23 +0900

    2022. 3. 30. PM 12:21 --------------------------------------- ADD - Camara OCR - --------------------------------------- Delete - - --------------------------------------- Error - Data path must contain subfolder tessdata - --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/activity_Camara_scan.java
+++ app/src/main/java/com/example/project_who_yak/activity_Camara_scan.java
@@ -7,7 +7,9 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -17,6 +19,16 @@ import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class activity_Camara_scan extends AppCompatActivity {
 
@@ -25,9 +37,14 @@ public class activity_Camara_scan extends AppCompatActivity {
     String mCurrentPhotoPath;
     final int REQUEST_TAKE_PHOTO = 1;
 
+    //OCR 변수
+    Bitmap image;
+    private TessBaseAPI mTess;
+    String datapath = "/Users/kimandrew/AndroidStudioProjects/Project_Who_yak/app/src/main/assets/";
+    TextView OCRTextview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         //화면 이동
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camara_scan);
@@ -38,6 +55,18 @@ public class activity_Camara_scan extends AppCompatActivity {
         btnBack = (Button)findViewById(R.id.btnBack);
         btnPic = (Button) findViewById(R.id.btnPic);
 
+        //OCR Textview
+        OCRTextview = findViewById(R.id.ocrResult);
+
+        //OCR 기능
+        image = BitmapFactory.decodeResource(getResources(), R.drawable.ocr_kr);
+        datapath = getFilesDir() + ""; // 언어파일 경로
+        checkFile(new File(datapath + "tessdata/")); // 트레이닝 데이터 확인
+        String lang = "kor"; // 언어 세팅
+        //OCR 세팅
+        mTess = new TessBaseAPI();
+        mTess.init(datapath, lang);
+
         //권한 확인
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
@@ -47,7 +76,6 @@ public class activity_Camara_scan extends AppCompatActivity {
                 Log.d(TAG, "권한 설정 요청");
                 ActivityCompat.requestPermissions(activity_Camara_scan.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1); }
         }
-
 
         //촬영 버튼 리스너
         btnPic.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +100,7 @@ public class activity_Camara_scan extends AppCompatActivity {
         });
     }
 
+
     // 권한 요청
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -83,7 +112,8 @@ public class activity_Camara_scan extends AppCompatActivity {
 
 
     // 카메라로 촬영한 사진의 썸네일을 가져와 이미지뷰에 띄워줌
-    @Override protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         ImageView ivPic;
         ImageView iv_preview1;
         ivPic = (ImageView)findViewById(R.id.ivPic);
@@ -99,6 +129,53 @@ public class activity_Camara_scan extends AppCompatActivity {
             }
         }break;
         }
+    }
+
+    //OCR 데이터 파일이 있는지 확인
+    private void checkFile(File dir) {
+        if (!dir.exists() && dir.mkdirs()){
+            copyFiles();
+        }
+        if (dir.exists()){
+            String datafilepath = "/tessdata/kor.traineddata";
+            File datafile = new File(datafilepath);
+            if(!datafile.exists()){
+                copyFiles();
+            }
+        }
+    }
+
+    private void copyFiles() {
+        try {
+            String filePath = "/tessdata/kor.traineddata";
+            AssetManager assetManager = getAssets();
+            InputStream instream = assetManager.open("tessdata/kor.traineddata");
+            OutputStream outstream = new FileOutputStream(filePath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1){
+                outstream.write(buffer,0,read);
+            }
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e){
+                e.printStackTrace();
+        }
+    }
+
+    //이미지에서 텍스트 읽기
+    public void  processImage(View view){
+        String OCResult = null;
+        mTess.setImage(image);
+        OCResult = mTess.getUTF8Text();
+        OCRTextview.setText(OCResult);
     }
 
 }
