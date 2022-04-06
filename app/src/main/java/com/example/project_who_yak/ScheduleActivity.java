e240b3c9566807857223e2d29102eee2c05efbc1
jyun23 <vjaos0619@naver.com>
2022-04-07 03:33:11 +0900

    2022. 4. 07. AM 03:32 --------------------------------------- ADD -build.gradle에 라이브러리 추가 -calender_selector.xml (background 모양을 변경할 때 사용할 드로어블)추가 -ScheduleActivity.java에 커스텀내용 추가 -strings.xml에 한글내용 추가 -themes.xml에 스타일 추가 -transparent_calendar_element.xml(calendar_selector.xml내용) --------------------------------------- Delete - - --------------------------------------- Error -스케줄화면이 보이지 않고 튕김 (app:mcv_selectionMode="range"을 작성하면 작동불가->에러떠서 작성 안함) - --------------------------------------- Fix -activity_schedule부분 CalendarView->MaterialCalendarView - --------------------------------------- ```
    -----------------

--- app/src/main/java/com/example/project_who_yak/ScheduleActivity.java
+++ app/src/main/java/com/example/project_who_yak/ScheduleActivity.java
@@ -1,17 +1,35 @@
 package com.example.project_who_yak;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter;
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter;
+import com.prolificinteractive.materialcalendarview.format.TitleFormatter;
+
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ScheduleActivity extends AppCompatActivity {
-    CalendarView calendarView;
+
+    private final String TAG = this.getClass().getSimpleName();
+    private MaterialCalendarView calendarView;
+
     Button btnvoice;
     Button btnhome;
     ListView lvCal;
@@ -25,6 +43,7 @@ public class ScheduleActivity extends AppCompatActivity {
         btnvoice = findViewById(R.id.btnvoice);
         btnhome = findViewById(R.id.btnhome);
 
+        calendarView = findViewById(R.id.calendarView);
 
         btnhome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +52,77 @@ public class ScheduleActivity extends AppCompatActivity {
 //                setContentView(R.layout.activity_home);
             }
         });
+
+
+        // 첫 시작 요일이 월요일이 되도록 설정
+        calendarView.state()
+                .edit()
+                .setFirstDayOfWeek(Calendar.MONDAY)
+                .commit();
+
+        // 월, 요일을 한글로 보이게 설정 (MonthArrayTitleFormatter의 작동을 확인하려면 밑의 setTitleFormatter()를 지운다)
+        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
+        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
+
+        // 좌우 화살표 사이 연, 월의 폰트 스타일 설정
+        calendarView.setHeaderTextAppearance(R.style.CalendarWidgetHeader);
+
+        // 요일 선택 시 내가 정의한 드로어블이 적용되도록 함
+        calendarView.setOnRangeSelectedListener(new OnRangeSelectedListener() {
+            @Override
+            public void onRangeSelected(@NonNull MaterialCalendarView widget, @NonNull List<CalendarDay> dates) {
+                // 아래 로그를 통해 시작일, 종료일이 어떻게 찍히는지 확인하고 본인이 필요한 방식에 따라 바꿔 사용한다
+                // UTC 시간을 구하려는 경우 이 라이브러리에서 제공하지 않으니 별도의 로직을 짜서 만들어내 써야 한다
+                String startDay = dates.get(0).getDate().toString();
+                String endDay = dates.get(dates.size() - 1).getDate().toString();
+                Log.e(TAG, "시작일 : " + startDay + ", 종료일 : " + endDay);
+            }
+        });
+
+        // 일자 선택 시 내가 정의한 드로어블이 적용되도록 한다
+        calendarView.addDecorators(new DayDecorator(this));
+
+        // 좌우 화살표 가운데의 연/월이 보이는 방식 커스텀
+        calendarView.setTitleFormatter(new TitleFormatter() {
+            @Override
+            public CharSequence format(CalendarDay day) {
+                // CalendarDay라는 클래스는 LocalDate 클래스를 기반으로 만들어진 클래스다
+                // 때문에 MaterialCalendarView에서 연/월 보여주기를 커스텀하려면 CalendarDay 객체의 getDate()로 연/월을 구한 다음 LocalDate 객체에 넣어서
+                // LocalDate로 변환하는 처리가 필요하다
+                Date inputText = day.getDate();
+                String[] calendarHeaderElements = inputText.toString().split("-");
+                StringBuilder calendarHeaderBuilder = new StringBuilder();
+                calendarHeaderBuilder.append(calendarHeaderElements[0])
+                        .append(" ")
+                        .append(calendarHeaderElements[1]);
+                return calendarHeaderBuilder.toString();
+            }
+        });
     }
+
+    /* 선택된 요일의 background를 설정하는 Decorator 클래스 */
+    private static class DayDecorator implements DayViewDecorator {
+
+        private final Drawable drawable;
+
+        public DayDecorator(Context context) {
+            drawable = ContextCompat.getDrawable(context, R.drawable.calendar_selector);
+        }
+
+        // true를 리턴 시 모든 요일에 내가 설정한 드로어블이 적용된다
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return true;
+        }
+
+        // 일자 선택 시 내가 정의한 드로어블이 적용되도록 한다
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.setSelectionDrawable(drawable);
+//            view.addSpan(new StyleSpan(Typeface.BOLD));   // 달력 안의 모든 숫자들이 볼드 처리됨
+        }
+    }
+
 }
+
+
