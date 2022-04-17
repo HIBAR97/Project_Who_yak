a227ae7c0306ff79a69f0b790b1222c46fb394ca
HIBER97 <hibar9710@gmail.com>
2022-04-17 22:35:45 +0900

    2022. 4. 17. PM 10:35 --------------------------------------- ADD - - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - OCR fungtion fixed? - ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/activity_Camara_scan.java
+++ app/src/main/java/com/example/project_who_yak/activity_Camara_scan.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -50,17 +50,18 @@ public class activity_Camara_scan extends AppCompatActivity {
         Button btnPic;
         Button btnBack;
 
-        btnBack = (Button)findViewById(R.id.btnBack);
+        btnBack = (Button) findViewById(R.id.btnBack);
         btnPic = (Button) findViewById(R.id.btnPic);
 
         //OCR Textview
-        OCRTextview = findViewById(R.id.ocrResult);
+        OCRTextview = (TextView) findViewById(R.id.ocrResult);
 
         //OCR 기능
-        image = BitmapFactory.decodeResource(getResources(), R.drawable.ocr_kr);
-        datapath = getFilesDir() + ""; // 언어파일 경로
-        checkFile(new File(datapath + "/tessdata/kor.traineddata")); // 트레이닝 데이터 확인
+        datapath = getFilesDir() + "/tesseract/"; // 언어파일 경로
+        //checkFile(new File(datapath + "/tessdata/kor.traineddata")); // 트레이닝 데이터 확인
+        checkFile(new File(datapath + "tessdata/"),"kor");
         String lang = "kor"; // 언어 세팅
+
         //OCR 세팅
         mTess = new TessBaseAPI();
         mTess.init(datapath, lang);
@@ -94,6 +95,10 @@ public class activity_Camara_scan extends AppCompatActivity {
             public void onClick(View v) {
                 //Main으로 이동
                 //finish();
+                BitmapDrawable d = (BitmapDrawable)((ImageView) findViewById(R.id.ivPic)).getDrawable();
+                image = d.getBitmap();
+
+                //image = BitmapFactory.decodeResource(getResources(), R.drawable.ocr_kr);
                 String OCResult = null;
                 mTess.setImage(image);
                 OCResult = mTess.getUTF8Text();
@@ -133,51 +138,90 @@ public class activity_Camara_scan extends AppCompatActivity {
         }
     }
 
-    //OCR 데이터 파일이 있는지 확인
-    private void checkFile(File dir) {
-        if (!dir.exists() && dir.mkdirs()){
-            copyFiles();
+
+//장치에 파일 복사
+private void copyFiles(String lang) {
+    try{
+        //파일이 있을 위치
+        String filepath = datapath + "/tessdata/"+lang+".traineddata";
+
+        //AssetManager에 액세스
+        AssetManager assetManager = getAssets();
+
+        //읽기/쓰기를 위한 열린 바이트 스트림
+        InputStream instream = assetManager.open("tessdata/"+lang+".traineddata");
+        OutputStream outstream = new FileOutputStream(filepath);
+
+        //filepath에 의해 지정된 위치에 파일 복사
+        byte[] buffer = new byte[1024];
+        int read;
+
+        while ((read = instream.read(buffer)) != -1) {
+            outstream.write(buffer, 0, read);
         }
-        if (dir.exists()){
-            String datafilepath = "/Users/kimandrew/AndroidStudioProjects/Project_Who_yak/app/src/main/assets/tessdata/kor.traineddata";
+        outstream.flush();
+        outstream.close();
+        instream.close();
+
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+    //check file on the device
+    private void checkFile(File dir, String lang) {
+        //디렉토리가 없으면 디렉토리를 만들고 그후에 파일을 카피
+        if (!dir.exists() && dir.mkdirs()) {
+            copyFiles(lang);
+        }
+        //디렉토리가 있지만 파일이 없으면 파일카피 진행
+        if (dir.exists()) {
+            String datafilepath = datapath + "/tessdata/" + lang + ".traineddata";
             File datafile = new File(datafilepath);
-            if(!datafile.exists()){
-                copyFiles();
+            if (!datafile.exists()) {
+                copyFiles(lang);
             }
         }
     }
 
-    private void copyFiles() {
-        try {
-            String filePath = "/tessdata/kor.traineddata";
-            //String filePath = "tessdata/";
-            AssetManager assetManager = getAssets();
-            //InputStream instream = assetManager.open("tessdata/kor.traineddata");
-            InputStream instream = assetManager.open("/Users/kimandrew/AndroidStudioProjects/Project_Who_yak/app/src/main/assets/tessdata/kor.traineddata");
-            OutputStream outstream = new FileOutputStream(filePath);
-
-            byte[] buffer = new byte[1024];
-            int read;
-            while ((read = instream.read(buffer)) != -1){
-                outstream.write(buffer,0,read);
-            }
-            outstream.flush();
-            outstream.close();
-            instream.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //이미지에서 텍스트 읽기
-    public void  processImage(View view){
-        String OCResult = null;
-        mTess.setImage(image);
-        OCResult = mTess.getUTF8Text();
-        OCRTextview.setText(OCResult);
-    }
-
+    //    //OCR 데이터 파일이 있는지 확인
+//    private void checkFile(File dir) {
+//        if (!dir.exists() && dir.mkdirs()){
+//            copyFiles();
+//        }
+//        if (dir.exists()){
+//            String datafilepath = "/Users/kimandrew/AndroidStudioProjects/Project_Who_yak/app/src/main/assets/tessdata/kor.traineddata";
+//            File datafile = new File(datafilepath);
+//            if(!datafile.exists()){
+//                copyFiles();
+//            }
+//        }
+//    }
+//
+//    private void copyFiles() {
+//        try {
+//            String filePath = "/tessdata/kor.traineddata";
+//            //String filePath = "tessdata/";
+//            AssetManager assetManager = getAssets();
+//            //InputStream instream = assetManager.open("tessdata/kor.traineddata");
+//            InputStream instream = assetManager.open("/Users/kimandrew/AndroidStudioProjects/Project_Who_yak/app/src/main/assets/tessdata/kor.traineddata");
+//            OutputStream outstream = new FileOutputStream(filePath);
+//
+//            byte[] buffer = new byte[1024];
+//            int read;
+//            while ((read = instream.read(buffer)) != -1){
+//                outstream.write(buffer,0,read);
+//            }
+//            outstream.flush();
+//            outstream.close();
+//            instream.close();
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
