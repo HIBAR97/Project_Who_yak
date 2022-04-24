201db9b0633237eaeaa9dbaf16ace775c314fd2f
jyun23 <vjaos0619@naver.com>
2022-04-24 13:51:54 +0900

    2022. 4. 24. PM 1:51 --------------------------------------- ADD - - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix -달력 날짜 토,일 색상 넣기 -월요일로 시작되던게 일요일로 시작되게 변경 --------------------------------------- ``` -----------------

---
+++ app/src/main/java/com/example/project_who_yak/SaturdayDecorator.java
@@ -0,0 +1,28 @@
+package com.example.project_who_yak;
+
+import android.graphics.Color;
+import android.text.style.ForegroundColorSpan;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+
+import java.util.Calendar;
+
+public class SaturdayDecorator implements DayViewDecorator {
+    private final Calendar calendar = Calendar.getInstance();
+    public SaturdayDecorator() {
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        day.copyTo(calendar);
+        int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+        return weekDay == Calendar.SATURDAY;
+    }
+
+    @Override
+    public void decorate(DayViewFacade viewFacade) {
+        viewFacade.addSpan(new ForegroundColorSpan(Color.BLUE));
+    }
+}
---
+++ app/src/main/java/com/example/project_who_yak/SundayDecorator.java
@@ -0,0 +1,29 @@
+package com.example.project_who_yak;
+
+import android.graphics.Color;
+import android.text.style.ForegroundColorSpan;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+
+import java.util.Calendar;
+
+public class SundayDecorator implements DayViewDecorator {
+    private final Calendar calendar = Calendar.getInstance();
+
+    public SundayDecorator() {
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        day.copyTo(calendar);
+        int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+        return weekDay == Calendar.SUNDAY;
+    }
+
+    @Override
+    public void decorate(DayViewFacade viewFacade) {
+        viewFacade.addSpan(new ForegroundColorSpan(Color.RED));
+    }
+}
