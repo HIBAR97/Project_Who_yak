cf85884b0f36b675ed3459c4651fea095b0ffe45
HIBER97 <hibar9710@gmail.com>
2022-05-11 22:09:18 +0900

    2022. 5. 11. PM 10:08 --------------------------------------- ADD - - --------------------------------------- Delete - - --------------------------------------- Error - board layout need xml - --------------------------------------- Fix - You can enter board layout - ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/fragment_bltboard.java
+++ app/src/main/java/com/example/project_who_yak/fragment_bltboard.java
@@ -1,19 +1,107 @@
 package com.example.project_who_yak;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class fragment_bltboard extends Fragment {
+
+    private ListView noticeListView;
+    private NoticeListAdapter adapter;
+    private List<Notice> noticeList;
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bltboard, container, false);
+
+        //선언
+        view = inflater.inflate(R.layout.fragment_bltboard, container, false);
+        noticeListView = (ListView) view.findViewById(R.id.listView);
+
+        noticeList = new ArrayList<Notice>();
+        adapter = new NoticeListAdapter(getActivity().getApplicationContext(), noticeList);
+        noticeListView.setAdapter(adapter);
+
+        new BackgrounTask().execute();
+        return view;
+    }
+
+    class BackgrounTask extends AsyncTask<Void, Void, String>
+    {
+        String target;
+
+        @Override
+        protected void onPreExecute() {
+            target = "http://whoyak.dothome.co.kr/BltboardNotice.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((temp = bufferedReader.readLine()) != null)
+                {
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values) {
+            super.onProgressUpdate();
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                int count = 0;
+                String noticeContent, noticeName, noticeDate;
+                while(count < jsonArray.length())
+                {
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    noticeContent = object.getString("noticeContent");
+                    noticeName = object.getString("noticeName");
+                    noticeDate = object.getString("noticeDate");
+                    Notice notice = new Notice(noticeContent, noticeName, noticeDate);
+                    noticeList.add(notice);
+                    count++;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
