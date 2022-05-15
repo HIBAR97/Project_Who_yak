ed35a4498d751d010da16111702b78c62300b6c7
HIBER97 <hibar9710@gmail.com>
2022-05-15 12:49:32 +0900

    2022. 5. 15. PM 12:48 --------------------------------------- ADD - activity_home.xml에 공지사항 listview 추가 - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/HomeActivity.java
+++ app/src/main/java/com/example/project_who_yak/HomeActivity.java
@@ -1,13 +1,27 @@
 package com.example.project_who_yak;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private ArrayList<Notice> noticeList;
 
     String userID;
     Button btnSearch;
@@ -16,6 +30,8 @@ public class HomeActivity extends AppCompatActivity {
     Button btnBoard;
     Button btnUserPage;
     //Button btnVoice;
+
+    ListView lv_notice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +47,8 @@ public class HomeActivity extends AppCompatActivity {
          //'Intent intentmain = getIntent();
         //String userID = intentmain.getStringExtra("userID");
         //btnVoice.setText(userID);
+
+        lv_notice = findViewById(R.id.lv_HomeNotice);
 
 
 
@@ -77,6 +95,71 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        new HomeActivity.BackgrounTask().execute();
     }
 
+    //--------공지사항 연동------------//
+    class BackgrounTask extends AsyncTask<Void, Void, String>
+    {
+        String target;
+
+        @Override
+        protected void onPreExecute() {
+            target = "http://whoyak.dothome.co.kr/BltboardNotice.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((temp = bufferedReader.readLine()) != null)
+                {
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values) {
+            super.onProgressUpdate();
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                int count = 0;
+                String noticeContent, noticeName, noticeDate;
+                noticeList = new ArrayList<Notice>();
+                while(count < jsonArray.length())
+                {
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    noticeContent = object.getString("noticeContent");
+                    noticeName = object.getString("noticeName");
+                    noticeDate = object.getString("noticeDate");
+                    Notice notice = new Notice(noticeContent, noticeName, noticeDate);
+                    noticeList.add(notice);
+                    final NoticeListAdapter NoticeAdapter = new NoticeListAdapter(getApplicationContext(), noticeList);
+                    lv_notice.setAdapter(NoticeAdapter);
+                    count++;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
