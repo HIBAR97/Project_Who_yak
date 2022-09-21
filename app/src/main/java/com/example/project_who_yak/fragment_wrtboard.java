1b5eeb4b49f436a689ba3b577c3001fa8bc11894
jaewook <jwjung0721@sungkyul.ac.kr>
2022-09-21 21:34:49 +0900

    2022. 9. 21. AM/PM 20:24 --------------------------------------- ADD - 게시글 작성 스피너 활성화 -   "   이미지 선택   " -   "    취소 버튼    " - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - '글 추가 버튼' 예정 - 게시글 '입장, 댓글, 삭제, 수정' 필요 ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/fragment_wrtboard.java
+++ app/src/main/java/com/example/project_who_yak/fragment_wrtboard.java
@@ -7,10 +7,13 @@ import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,11 +22,15 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.project_who_yak.databinding.ActivityUserpageBinding;
 
+import java.util.ArrayList;
+
 public class fragment_wrtboard extends Fragment {
 
     private View view;
     private final int GET_GALLERY_IMAGE = 200;
     private ImageView imageView;
+    private Spinner spinner1;
+    private static final String[] genre = new String[]{"자유", "질문", "공지", "정보"};
 
     @Nullable
     @Override
@@ -34,6 +41,12 @@ public class fragment_wrtboard extends Fragment {
         Button btnadd = (Button) view.findViewById(R.id.blt_brd);
         Button btncnl = (Button) view.findViewById(R.id.blt_rmb);
         Button btnfil = (Button) view.findViewById(R.id.fl_plus);
+
+        spinner1 = (Spinner) view.findViewById(R.id.spn_main);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item,genre);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
         btnadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
