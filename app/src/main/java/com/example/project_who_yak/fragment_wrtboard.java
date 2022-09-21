0f033e696fa5698f762187e5207735f5ebf56053
jaewook <jwjung0721@sungkyul.ac.kr>
2022-09-21 20:25:55 +0900

    2022. 9. 21. AM/PM 20:24 --------------------------------------- ADD - 게시판 버튼 활성화 - - - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - 추가 버튼, 스피너 구현 중 - ---------------------------------------

--- app/src/main/java/com/example/project_who_yak/fragment_wrtboard.java
+++ app/src/main/java/com/example/project_who_yak/fragment_wrtboard.java
@@ -1,30 +1,64 @@
 package com.example.project_who_yak;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.project_who_yak.databinding.ActivityUserpageBinding;
 
 public class fragment_wrtboard extends Fragment {
 
     private View view;
-
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_wrtboard, container, false);
 
-//        Button btnadd = view.findViewById(R.id.blt_brd);
-//        Button btncnl = view.findViewById(R.id.blt_rmb);
-//        Button btnfil = view.findViewById(R.id.fl_plus);
+        view = inflater.inflate(R.layout.fragment_wrtboard, container, false);
 
+        Button btnadd = (Button) view.findViewById(R.id.blt_brd);
+        Button btncnl = (Button) view.findViewById(R.id.blt_rmb);
+        Button btnfil = (Button) view.findViewById(R.id.fl_plus);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
+            }
+        });
+        btncnl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),activity_bltboard.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+        btnfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mintent = new Intent(Intent.ACTION_PICK);
+                mintent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+                startActivityForResult(mintent,GET_GALLERY_IMAGE);
+                //갤러리 열기
+//                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+//                startActivity(mIntent);
+            }
+        });
+        return view;
     }
 }
